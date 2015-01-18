26ca3d47bc2d1123908e906c0e1b3220b0678432
fedepira <federicopiragua12@hotmail.com>
2015-01-18 01:33:36 -0500

    Update start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -1,8 +1,4 @@
 <?php
-require_once 'whoops/vendor/autoload.php';
-$whoops = new Whoops\Run();
-$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
-$whoops->register(); 
 
 require_once '../vendor/autoload.php';
 require_once 'app.php';
